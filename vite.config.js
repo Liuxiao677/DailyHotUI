3cd9a7e312ef84992ae23c6cf7465250acfb01d2
Liuxiao6 <25676419+Liuxiao677@users.noreply.github.com>
2024-12-22 14:52:56 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -52,9 +52,9 @@ export default defineConfig({
         ],
       },
       manifest: {
-        name: "每日新闻-今日热榜",
+        name: "每日新闻-即时新闻速递",
         short_name: "MeiRINews",
-        description: "汇聚全网热点，热门尽览无余",
+        description: "每日新闻（Www.Meiri.News）是您获取最新、最全面新闻资讯的首选平台。我们致力于提供实时、权威的全球新闻报道，涵盖政治、经济、科技、体育及娱乐等各个领域。无论您身处何地，每日新闻都能让您第一时间掌握世界动态，享受新闻阅读的乐趣。我们的团队由资深记者和编辑组成，确保每一条新闻都经过严格审核，为您提供最准确、最深入的新闻分析和评论。加入我们，让每日新闻成为您生活中不可或缺的一部分。",
         display: "standalone",
         start_url: "/",
         theme_color: "#fff",
